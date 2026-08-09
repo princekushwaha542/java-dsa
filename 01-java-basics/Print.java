2cb63c3d6cb569eb2fc090d19866dbaef914aab3
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-08-09 22:11:19 +0530

    feat: implement user name input and output

--- 01-java-basics/Print.java
+++ 01-java-basics/Print.java
@@ -1,5 +1,13 @@
+import java.util.*;
+
 public class Print {
     public static void main(String[] args) {
+        //Input
+        Scanner input = new Scanner(System.in);
+        System.out.print( "Enter your name : ");
+        String name = input.nextLine();
+        System.out.println(name);
+        //Output
         int a = 5;
         System.out.print("Hello, Java!");
         System.out.print("The value of a is: " + a);
