adb39f3df41a122b22c528c8c18821c2a56103f9
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-08-10 23:14:34 +0530

    feat: implement age classification checker using Scanner class

--- 01-java-basics/Print.java
+++ 01-java-basics/Print.java
@@ -3,11 +3,19 @@ import java.util.*;
 public class Print {
     public static void main(String[] args) {
         //Input
+        
         Scanner input = new Scanner(System.in);
         System.out.print( "Enter your name : ");
         String name = input.nextLine();
         System.out.println(name);
+        // All data type can be taken a input from user using Scanner class
+        //      int number = input.nextInt();
+        //      String name = input.nextLine();   
+        //      char letter = input.next().charAt(0);
+        //      double preciseValue = input.nextDouble();
+        //      float piValue = input.nextFloat();
         //Output
+
         int a = 5;
         System.out.print("Hello, Java!");
         System.out.print("The value of a is: " + a);
