c24d7a97e5ecc953040a855ea62f40de5e81fc30
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-08-12 22:59:40 +0530

    feat : implement loops and conditional program

---
+++ 01-java-basics/Looop.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Looop {
+    public static void main(String[] arguments) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1; i<=n; i++){
+            System.out.println(i);
+        }   
+    }
+}
