bb79db0331d8b732682372f100a3e4ba82722deb
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-08-15 22:22:40 +0530

    feat: add solid rectangle pattern using nested loops

---
+++ 01-java-basics/SolidRectangle.java
@@ -0,0 +1,12 @@
+
+public class SolidRectangle{
+    public static void main(String[] argument ){
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=5; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+ 
