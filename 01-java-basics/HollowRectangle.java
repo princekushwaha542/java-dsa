3107841debbbfa94abfbf29f56ff0fed2f7e53dd
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-08-16 23:01:16 +0530

    feat: add hollow rectangle pattern using nested loops

---
+++ 01-java-basics/HollowRectangle.java
@@ -0,0 +1,23 @@
+ import java.util.*;
+public class HollowRectangle {
+    public static void main(String[] args) {
+
+        int rows = 5;
+        int cols = 8;
+
+        for (int i = 1; i <= rows; i++) {
+
+            for (int j = 1; j <= cols; j++) {
+
+                // Boundary condition
+                if (i == 1 || i == rows || j == 1 || j == cols) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
