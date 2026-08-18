2cc6fa5defdb4923659ade69415f726cd6bd21da
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-08-18 22:01:40 +0530

    feat: add butterfly diamond pattern

---
+++ 01-java-basics/diamond.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class diamond {
+    public static void main(String[] args) {
+
+        int n = 8;
+
+        // Upper part
+        for (int i = 1; i <= n; i++) {
+
+            // Left stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // Spaces
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+
+            // Right stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        // Lower part
+        for (int i = n; i >= 1; i--) {
+
+            // Left stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // Spaces
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+
+            // Right stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
