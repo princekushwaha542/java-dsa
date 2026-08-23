11a6e56ed33fd61cb2b51913eae7b57a8f29b990
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-08-23 23:29:30 +0530

    feat : practice function and  create a program

---
+++ 01-java-basics/function.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class function{
+    public static void PrintName(String name){
+        System.out.print(name);
+    }
+
+    public static  void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    String name = scanner.next();
+    PrintName(name);
+    }
+}
+
+
