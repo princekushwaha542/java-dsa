44260345c4b96b380f2d7f13e05c592d9f4ac4cc
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-08-25 23:38:51 +0530

    feat : implement  sm and average program

---
+++ 01-java-basics/ArraySumAverage.java
@@ -0,0 +1,15 @@
+public class ArraySumAverage {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        int sum = 0;
+
+        for (int num : numbers) {
+            sum += num;
+        }
+
+        double average = (double) sum / numbers.length;
+
+        System.out.println("एरे का योग (Sum): " + sum);
+        System.out.println("एरे का औसत (Average): " + average);
+    }
+}
