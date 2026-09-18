ef936423e23e820da0c41f562b51830f8e38721b
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-09-18 22:59:50 +0530

    Implement selection sort in Java

---
+++ 01-java-basics/string/SelectionSort.java
@@ -0,0 +1,34 @@
+package array;
+
+import java.util.*;
+
+public class SelectionSort {
+
+    public static void main(String[] args) {
+
+        int[] arr = {64, 25, 12, 22, 11};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            int minIndex = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+
+        System.out.println("Sorted Array:");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
